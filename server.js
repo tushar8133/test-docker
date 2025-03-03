72ce08c7677a2861a01205931c23d3ce5c1e562a
tushar8133 <tushar8133@gmail.com>
2025-03-04 00:45:03 +0530

    http interceptor

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const fs = require("fs");
 require("dotenv").config();
 
+/*
+200 Ok
+201 Resource Created
+400 Bad Request
+401 Unauthorized
+404 Not Found
+500 Internal Server Error
+res.status // 400
+res.statusText // Not found
+res.ok // 2XX
+await res.json()
+await res.text()
+*/
+
+const COOKIE_TIMEOUT = 10 * 1000;
 const { PORT, MONGO_PORT, MONGO_URL, MONGO_USER, MONGO_PASSWORD } = process.env;
 
 mongoose.connect(
@@ -30,58 +46,84 @@ const DynamicModel = mongoose.model(
 let sessionDetails = [];
 let usersCount = 0;
 
-function cookieParser(req, res, next) {
-
-  if (req.url === "/login") {
-    return next();
-  }
-  
-  if (!req.headers.cookie) {
-    console.log('INVALID COOKIE');
-    res.status(404).end("Invalid Cookie");
-    return;
-  }
-
-  const [cookName, cookValue] = req.headers?.cookie?.split("=");
-  
+function checkAndUpdateCookie(req, res, next) {
+  const [cookName = null, cookValue = null] = req.headers?.cookie?.split("=") ?? [];
   if (cookName === "user" && sessionDetails.includes(Number(cookValue))) {
     console.log("existing user", cookValue);
     res.cookie("user", cookValue, {
       httpOnly: true,
       secure: false,
       sameSite: 'strict',
-      maxAge: 10000,
+      maxAge: COOKIE_TIMEOUT,
     });
-    return next();
   }
-  
-};
+  next();
+}
+
+function ifCookieNotPresent(req, res, next) {
+  console.log(req.url)
+
+  if (["/authenticate", "/login", "/logout", "/favicon.ico"].includes(req.url)) {
+    next();
+    return;
+  }
+
+  const [cookName = null, cookValue = null] = req.headers?.cookie?.split("=") ?? [];
+  if (cookName === "user" && sessionDetails.includes(Number(cookValue))) {
+    next();
+    return;
+  }
+
+  res.status(401).end("Invalid cookie");
+  return;
+}
+
+function addCookie(res) {
+  sessionId = ++usersCount;
+  sessionDetails.push(sessionId);
+  res.cookie("user", sessionId, {
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+    maxAge: COOKIE_TIMEOUT,
+  });
+  console.log("new user", sessionId);
+}
+
+function removeCookie(res) {
+  res.clearCookie('user');
+}
+
 
 const app = express();
 
-app.use(cookieParser);
-
 app.use(express.json());
-
 app.use("/", express.static("public"));
+app.use(checkAndUpdateCookie);
+app.use(ifCookieNotPresent);
+
+
 
 // POST endpoint to create a dynamic document
-app.get("/login", async (req, res) => {
+app.post("/authenticate", async (req, res) => {
   try {
-    if (!req.headers.cookie) {
-      sessionId = ++usersCount;
-      sessionDetails.push(sessionId);
-      res.cookie("user", sessionId, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-        maxAge: 10000,
-      });
-      console.log("new user", sessionId);
+    if (req.body.username === "aa" && req.body.password === "bb") {
+      addCookie(res);
+      res.json("Login Successful!");
+    } else {
+      res.status(401).json("Invalid credentials");
     }
-    res.status(201).json("Login Successful!");
   } catch (error) {
     res.status(500).json({ error: "Invalid Login" });
+  }
+});
+
+app.get("/logout", async (req, res) => {
+  try {
+    removeCookie(res);
+    res.json("User logged out!");
+  } catch (error) {
+    res.status(500).json({ error: "Problem in logging out!" });
   }
 });
 
